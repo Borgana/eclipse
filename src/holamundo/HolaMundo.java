a523695331bee028e0a33a7d098828196402c245
Borgana <jorge.borrod.1@educa.jcyl.es>
2023-04-14 20:26:09 +0200

    haciendo mi primer commit

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,7 +4,11 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hola mundo");
+		System.out.println("hola mi primer cambia");
+		System.out.println("hola mi segundo cambia1");
+		System.out.println("hola mi segundo cambia2");
+		System.out.println("hola mi segundo cambia3");
+		System.out.println("hola mi segundo cambia3");
 	}
 
 }
